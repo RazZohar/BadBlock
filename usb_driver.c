0f98b8c357a18638cd45b1ca3022c4f5d5df4e6c
Raz <razzohar12@gmail.com>
2016-04-17 11:55:43 +0300

    Add log meachnism

--- usb_driver.c
+++ usb_driver.c
@@ -3,6 +3,15 @@
 #include <linux/kernel.h>
 #include <linux/usb.h>
 
+#include <linux/types.h>
+
+#include <linux/fs.h>
+#include <linux/string.h>
+#include <asm/uaccess.h> 
+
+#include <linux/time.h>
+
+
 //Mass storage device
 #define B_INTERFACE_CLASS 8
 
@@ -12,14 +21,71 @@
 //BULK only Protocol
 #define B_INTERFACE_PROTOCOL 80
 
-const int bInterfaceClass = 8;	
-const int bInterfaceSubClass = 6;	//SCSI Sub class interface
+#define NUM_OF_BYTE_BUFF 256
+
+MODULE_AUTHOR("Raz Zohar");
+MODULE_DESCRIPTION("Module for Genral DOK");
+MODULE_LICENSE("GPL");
+
+// //char * product;
+//   char * manufacturer;
+//   char * serial;
+
+char buffer[NUM_OF_BYTE_BUFF] = {0};
+static struct file *log_file = NULL;
+
+//---------------------------------------------------------------------------------------------------
+static void write(struct file * filp, char * buffer)
+{
+	//Write to file
+	filp->f_op->write(filp, buffer, strlen(buffer), &filp->f_pos);
+	
+
+}
+
+static void log(struct file * log_files, char * file_name)
+{
+	mm_segment_t old_fs;
+
+	log_files = filp_open(file_name, O_RDWR | O_CREAT, 0644);
+
+	if(IS_ERR(log_files))
+	{
+		printk("open error...\n");
+
+	}
+	
+
+	old_fs = get_fs();
+	set_fs(get_ds());
 
 
+	write(log_files, buffer);
+
+	filp_close(log_files, NULL);
+}
+
+
+
+
+//---------------------------------------------------------------------------------------------------
 //When The USB device is plug in
 static int usb_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
-	printk("USB device is plugged in\n");
+	struct usb_device * usb_device_data = interface_to_usbdev(interface);
+
+	char file_name[NUM_OF_BYTE_BUFF];
+
+	sprintf(file_name, "/root/%s_log.txt", usb_device_data->product);
+
+	printk("log file name:%s\n", file_name);
+	printk("USB device is plugged in with product name:%s \n", usb_device_data->product);
+
+	sprintf(buffer, "USB Device Connected:\nProduct Name:%s\t Serial Number:%s\n", usb_device_data->product, usb_device_data->serial);
+
+	log(log_file, file_name);
+
+
 	return 0; 
 }
 
@@ -74,4 +140,3 @@ static void __exit usb_exit(void)
 
 module_init(usb_init);
 module_exit(usb_exit);
-MODULE_LICENSE("GPL");
